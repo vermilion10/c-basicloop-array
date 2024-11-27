984c3cd9a572dd9603b5e1959a97b4bc49aad5c5
vermilion10 <fathinyassarahmaan@gmail.com>
2024-11-27 22:42:57 +0700

    s

--- tempCodeRunnerFile.c
+++
@@ -1,7 +0,0 @@
-if (N == 100){
-        if (pjgK == pjgL && persentaseKesamaanK == 100 && persentaseKesamaanL == 100){
-            printf ("sama\n");
-        } else {
-            printf("tidak sama\n");
-        }
-    } else
